083be9b23743f3052bf3c0158ba6349d86d813d7
Melanka2 <74191749+Melanka2@users.noreply.github.com>
2021-10-16 14:09:35 +0530

    Update Program.c
    Made changes to Program.c

--- Program.c
+++ Program.c
@@ -4,15 +4,15 @@ int main()
 {
     char name,country,skill;
     printf("Welcome to Hacktoberfest 2020\n");
-    printf("Enter your name : ");//Get User name
+    printf("Enter your name : ");//Get the User's Name
     scanf("%d",&name);
-    printf("Enter your Country of Residance : ");//Get user country
+    printf("Enter your Country of Residance : ");//Get the User's Country
     scanf("%s",&country);
-    printf("Enter your Age : ");//Get user Age
+    printf("Enter your Age : ");//Get User's Age 
     printf("%f",&age);
-    printf("Rate your programmings skills from A-D (A being the highest and D being the lowest : ");//Get user skill level
+    printf("Rate your programmings skills from A-D (A being the highest and D being the lowest : ");//Get user's Programming skills
     scanf(" %c",skill);
-    scanf("Thank you %c Happy Hacking",country);//Thank the user
+    scanf("Thank you %c Happy Hacking",country);//Thanking the user
 
     return 0;
 }
